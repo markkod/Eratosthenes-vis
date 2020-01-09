0c00d2ac752c174896b3958bdbfece8db1345f1b
markkod <mark.kodar@gmail.com>
2020-01-09 22:38:27 +0200

    remove todo

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const primeRange = (start, end) =>
     .fill()
     .map((_, idx) => start + idx);
 
-// needs to be made more efficient as each indexOf and filter operations is O(n)...
 function eratosthenes(n) {
   var p = 2;
   var range = primeRange(p, n);
