22ccb185b36b4d5d3adfda9fbecf780be491b923
markkod <mark.kodar@gmail.com>
2020-01-14 11:06:01 +0200

    still merge issues

--- src/utils/range.js
+++ src/utils/range.js
@@ -5,12 +5,6 @@ export const getArrayOfRange = (start, end) =>
 
 
 export const ALGORITHMS = {
-<<<<<<< HEAD
   ERRATOSTHENES: "ERRATOSTHENES",
   SUNDARAM: "SUNDARAM"
-=======
-  ERATOSTHENES: "ERATOSTHENES",
-  EULER: "EULER",
-  ATKINS: "ATKINS",
->>>>>>> 66dc75456a9024ce80ba9b90d6b6370d3407e5fb
 }
