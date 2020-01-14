d76fc03a73acfdf80bd85346e60ee73281c5ea75
markkod <mark.kodar@gmail.com>
2020-01-14 21:41:05 +0200

    Updates

---
+++ precache-manifest.9f66ac66004d4727645d3de852a03f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d910d88ffe1cbac8f39da0ce8fc64169",
+    "url": "/Eratosthenes-vis/index.html"
+  },
+  {
+    "revision": "c9d32cc09928d76115f0",
+    "url": "/Eratosthenes-vis/static/css/main.262383ed.chunk.css"
+  },
+  {
+    "revision": "523e452346e38ba46274",
+    "url": "/Eratosthenes-vis/static/js/2.83cff416.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/Eratosthenes-vis/static/js/2.83cff416.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9d32cc09928d76115f0",
+    "url": "/Eratosthenes-vis/static/js/main.2fb3cc86.chunk.js"
+  },
+  {
+    "revision": "25ab10d771835b9433d4",
+    "url": "/Eratosthenes-vis/static/js/runtime-main.b4736547.js"
+  }
+]);
